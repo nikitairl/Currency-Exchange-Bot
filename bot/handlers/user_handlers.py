dc3ff7c77532dab201742d6dece27875a93e798b
Enoth <58894013+nikitairl@users.noreply.github.com>
2024-07-23 15:14:18 +0300

    latest working version

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -1,6 +1,8 @@
+import logging
+
 from aiogram import Router
-from aiogram.types import Message
 from aiogram.filters import Command
+from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup
 from dotenv import load_dotenv
 
 from db import REDIS_CLIENT
@@ -9,6 +11,30 @@ load_dotenv()
 
 
 user_router = Router()
+
+
+@user_router.message(Command(commands=["start"]))
+async def start(message: Message):
+    buttons = [
+        [KeyboardButton(text="/rates")],
+        [KeyboardButton(text="/exchange_guide")],
+    ]
+    keyboard = ReplyKeyboardMarkup(
+        is_persistent=True, keyboard=buttons, resize_keyboard=True
+    )
+    await message.answer(
+        "Welcome! Use the buttons below to get started.\n"
+        "This bot can show you current exchange rates.\n"
+        "Exchange rates updated every 20:00.\n",
+        reply_markup=keyboard,
+    )
+
+
+@user_router.message(Command(commands=["exchange_guide"]))
+async def send_guide(message: Message):
+    await message.answer(
+        "To exchange currency, use: \n/exchange FROM TO AMOUNT"
+    )
 
 
 @user_router.message(Command(commands=["rates"]))
@@ -25,14 +51,18 @@ async def send_exchange_rates(message: Message):
 async def convert_currency(message: Message):
     try:
         _, from_currency, to_currency, amount = message.text.split()
+        await message.reply(
+            f"Converting {amount} {from_currency} -> {to_currency}."
+        )
         amount = float(amount)
         from_rate = float(REDIS_CLIENT.get(from_currency))
         to_rate = float(REDIS_CLIENT.get(to_currency))
         result = amount * from_rate / to_rate
-        await message.reply(
+        await message.answer(
             f"{amount} {from_currency} = {result:.2f} {to_currency}"
         )
     except Exception:
-        await message.reply(
-            "Неверная команда. Используйте формат: /exchange USD RUB 10"
+        logging.exception("Failed to convert currency")
+        await message.answer(
+            "Wrong format or unknown currency. Example: /exchange USD BYN 10"
         )
--- bot/parser.py
+++ bot/parser.py
@@ -1,7 +1,8 @@
 import asyncio
+import logging
 import os
 import xml.etree.ElementTree as ET
-import logging
+
 from aiohttp import ClientSession
 from dotenv import load_dotenv
 
@@ -14,28 +15,39 @@ PARSE_URL = os.getenv("PARSE_URL")
 
 async def fetch_exchange_rates():
     url = PARSE_URL
+    logging.info(f"Fetching exchange rates from {url}")
     async with ClientSession() as session:
         async with session.get(url) as response:
+            logging.info("Exchange rates fetched successfully")
             return await response.text()
 
 
 def parse_exchange_rates(xml_data):
-    pre_text = ET.fromstring(xml_data)
-    rates = {}
-    for valute in pre_text.findall("Valute"):
-        char_code = valute.find("CharCode").text
-        value = valute.find("Value").text.replace(",", ".")
-        nominal = valute.find("Nominal").text
-        rates[char_code] = float(value) / float(nominal)
-    return rates
+    logging.info("Parsing exchange rates")
+    try:
+        pre_text = ET.fromstring(xml_data)
+        rates = {}
+        for valute in pre_text.findall("Valute"):
+            char_code = valute.find("CharCode").text
+            value = valute.find("Value").text.replace(",", ".")
+            nominal = valute.find("Nominal").text
+            rates[char_code] = float(value) / float(nominal)
+        logging.info("Exchange rates parsed successfully")
+        return rates
+    except ET.ParseError as e:
+        logging.error(f"Error parsing XML data: {e}")
+        return {}
 
 
 async def update_redis_rates():
+    logging.info("Updating Redis with new exchange rates")
     xml_data = await fetch_exchange_rates()
     rates = parse_exchange_rates(xml_data)
     for char_code, rate in rates.items():
         REDIS_CLIENT.set(char_code, rate)
+    logging.info("Redis update completed")
     return
+
 
 if __name__ == "__main__":
     asyncio.run(update_redis_rates())
